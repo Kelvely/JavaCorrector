602481d9089880ae35c1bf172ef15fd998dcee36
Kelvely <Kelvely@inkme.cc>
2017-06-22 13:30:11 +0800

    Rollback

--- src/cc/flintstone/javacorrector/correctors/ClassNameCorrector.java
+++ src/cc/flintstone/javacorrector/correctors/ClassNameCorrector.java
@@ -10,36 +10,8 @@ public class ClassNameCorrector implements Corrector {
 
 	@Override
 	public boolean correct(List<BClass> classes) {
-		for (BClass bClass : classes) {
-
-			FileInputStream in = new FileInputStream(bClass.getClassFile());
-
-	        CompilationUnit cu = JavaParser.parse(in);
-
-	        String ClassName = new MethodVisitor().visit(cu, null);
-	        
-	        if(isFirstLetterUpperCase(ClassName)) return true;
-	        else return false;
-		}
+		// TODO 
+		return false;
 	}
 	
-	private static class MethodVisitor extends VoidVisitorAdapter<Void> {
-        @Override
-        public String visit(MethodDeclaration n, Void arg) {
-            super.visit(n, arg);
-            return n.getName();
-        }
-    }
-
-	public int isFirstLetterUpperCase(String word){
-		word = word.trim();
-		
-		if(word == null||word.length()<=0) return -1;
-		
-		if(word.substring(0, 1).toCharArray()[0]>='A'&& word.substring(0, 1).toCharArray()[0]<='Z'){
-			return 1;
-		} else {
-			return 0;
-		}
-	}
 }
