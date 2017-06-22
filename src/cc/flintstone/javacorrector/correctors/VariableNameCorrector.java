b2a17580f366313d6f26a1b65bc5b07ae962173c
Kelvely <Kelvely@inkme.cc>
2017-06-22 13:24:52 +0800

    New function

---
+++ src/cc/flintstone/javacorrector/correctors/VariableNameCorrector.java
@@ -0,0 +1,15 @@
+package cc.flintstone.javacorrector.correctors;
+
+import java.util.List;
+
+import bluej.extensions.BClass;
+
+public class VariableNameCorrector implements Corrector {
+
+	@Override
+	public boolean correct(List<BClass> classes) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
