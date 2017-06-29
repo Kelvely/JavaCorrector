6229ed1fca24289644faa36baeff43c3bd9e9d05
Kelvely <Kelvely@inkme.cc>
2017-06-29 16:25:00 +0800

    Misc commit

--- src/cc/flintstone/javacorrector/JavaCorrector.java
+++ src/cc/flintstone/javacorrector/JavaCorrector.java
@@ -1,5 +1,9 @@
 package cc.flintstone.javacorrector;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +18,29 @@ import cc.flintstone.javacorrector.correctors.VariableNameCorrector;
 import cc.flintstone.javacorrector.listeners.CompileBroadcaster;
 
 public class JavaCorrector extends Extension {
+	
+	static {
+		try {
+			Class.forName("com.github.javaparser.JavaParser");
+		} catch (ClassNotFoundException ex) {
+			URL javaparserUrl = JavaCorrector.class.getResource("javaparser-core-3.2.9-SNAPSHOT.jar");
+			try {
+				Method urlAdder = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+				boolean accessibility = urlAdder.isAccessible();
+			
+			
+				if(!accessibility) {
+					urlAdder.setAccessible(true);
+				}
+				
+				URLClassLoader classloader = (URLClassLoader)ClassLoader.getSystemClassLoader();
+				urlAdder.invoke(classloader, javaparserUrl);
+			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | 
+					IllegalArgumentException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	private BlueJ blueJ;
 	private CompileBroadcaster broadcaster;
